f709268540eff2dd5eae7e7afdd0134dc68890b5
Rares Borcea <raresborcea@gmail.com>
2019-11-04 20:09:48 +0200

    Update and rename tema1.asm to evaluator.asm

--- evaluator.asm
+++ evaluator.asm
@@ -16,85 +16,84 @@ CMAIN:
     mov ebp, esp
 
     GET_STRING expr, MAX_INPUT_SIZE
-
-    ; your code goes here
+    
     xor ecx, ecx ;pozitia curenta in string
     xor esi, esi ;1-e negativ, 0-e pozitiv
     
 general_loop:
     xor edx, edx
-    ;se analizeaza fiecare caracter
+    ; se analizeaza fiecare caracter
     mov dl, byte[expr + ecx]
     
-    ;ma aflu pe caracterul terminator?
+    ; ma aflu pe caracterul terminator?
     cmp dl, 0
     je exit
     
-    ;este vorba despre un nr. negativ?
+    ; este vorba despre un nr. negativ?
     cmp dl, '-'
     je is_neg?
     
-    ;este vorba de un alt operator?
+    ; este vorba de un alt operator?
     cmp dl, '0'
     jb sign
     
-    ;altfel este nr. pozitiv
+    ; altfel este nr. pozitiv
     mov eax, edx
-    ;obtin cifra din cod ASCII
+    ; obtin cifra din cod ASCII
     sub eax, '0'
-    ;conditie nr. pozitiv
+    ; conditie nr. pozitiv
     xor esi, esi 
     jmp get_nr
     
 is_neg?:
     inc ecx
-    ;verific daca urmeaza cifra, spatiu sau terminator
+    ; verific daca urmeaza cifra, spatiu sau terminator
     mov dl, byte[expr + ecx]
     cmp dl, ' '
     je sub_sign
     cmp dl, 0
     je sub_sign
-    ;altfel e nr. negativ
+    ; altfel e nr. negativ
     mov eax, edx
-    ;obtin cifra din cod ASCII
+    ; obtin cifra din cod ASCII
     sub eax, '0'
-    ;conditie nr. negativ
+    ; conditie nr. negativ
     mov esi, 1
     
-    ;obtin restul nr. 
+    ; obtin restul nr. 
 get_nr:
     inc ecx
     xor edx, edx
     mov dl, byte[expr + ecx]
     
-    ;verific daca am terminat citirea nr.
+    ; verific daca am terminat citirea nr.
     cmp dl, 0
     je add_sign
     cmp dl, ' '
     je add_sign
     
-    ;altfel adaug noua cifra
+    ; altfel adaug noua cifra
     imul eax, 10
     sub edx, '0'
     add eax, edx
     jmp get_nr
 add_sign:
-    ;adaug semnul daca e necesar
+    ; adaug semnul daca e necesar
     cmp esi, 1
     jne push_to_stack
     neg eax
     jmp push_to_stack 
     
 sub_sign:
-    ;scadere
+    ; scadere
     pop ebx
     pop eax
     sub eax, ebx
     jmp push_to_stack
     
 sign: 
-    ;celelalte semne
-    ;incrementez pozitia
+    ; celelalte semne
+    ; incrementez pozitia
     inc ecx
     cmp dl, '+'
     je sum_sign
@@ -104,45 +103,45 @@ sign:
     je div_sign
 
 sum_sign:
-    ;adunare
+    ; adunare
     pop ebx
     pop eax
     add eax, ebx
     jmp push_to_stack
     
 mul_sign:
-    ;inmultire
+    ; inmultire
     pop ebx
     pop eax
     imul ebx
     jmp push_to_stack
 
 div_sign:
-    ;impartire
+    ; impartire
     pop ebx
     pop edx
     xor eax, eax
     mov eax, edx
-    ;extindem semnul si in edx
+    ; extindem semnul si in edx
     cdq
     idiv ebx
     jmp push_to_stack
     
 push_to_stack:
-    ;adaugarea rezultatului in stiva
+    ; adaugarea rezultatului in stiva
     push eax 
-    ;verific daca ma aflu pe spatiu sau pe terminator
+    ; verific daca ma aflu pe spatiu sau pe terminator
     xor edx, edx
     mov dl, byte[expr + ecx]
     cmp dl, ' '
     jne next
-    ;daca sunt pe spatiu, incrementez pozitia
+    ; daca sunt pe spatiu, incrementez pozitia
     inc ecx
 next:
     jmp general_loop
        
 exit:    
-    ;afisez rezultatul
+    ; afisez rezultatul
     pop eax
     PRINT_DEC 4, eax
     NEWLINE
